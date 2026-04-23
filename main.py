e0cbc168f4a71199bdd9042082db31ff1ea63223
Matte447 <matteo.bienert@icloud.com>
2026-04-23 16:56:40 +0000

    adding a option if the user wants historical data or only the current price

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ print(get_portfolio())
 
 # getting user info about which stock, interval and start/end date
 company_stock_suffix = input("What stock do you want to look at? ")
-stock_interval = input(
-    "In what interval do you want your stocks? (1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo) "
-)
-stock_start = input("When should the data start? (yyyy-mm-dd) ")
-stock_end = input("When should the data end? (yyyy-mm-dd) ")
+what_data = input("Do you want historical data? (y/n) ")
+if what_data.lower() == "y" or what_data.lower() == "yes":
+    stock_interval = input(
+        "In what interval do you want your stocks? (1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo) "
+    )
+    stock_start = input("When should the data start? (yyyy-mm-dd) ")
+    stock_end = input("When should the data end? (yyyy-mm-dd) ")
 
-# getting stock history
-print(get_stock_history(company_stock_suffix, stock_interval, stock_start, stock_end))
+    # getting stock history
+    print(get_stock_history(company_stock_suffix, stock_interval, stock_start, stock_end))
 
 # getting current stock price
-print(get_current_stock_price(company_stock_suffix))
+print(f"Current price: {get_current_stock_price(company_stock_suffix)}")
